bbf4604ed47067f8023b786f4de77bcf873da821
stanstaton <thatkidenzo@gmail.com>
2019-07-24 16:32:59 -0700

    new switch statement for grade.js

--- js/grade.js
+++ js/grade.js
@@ -12,24 +12,41 @@ if (grade >= 90) {
 	console.log("Your grade is an F")
 }
 
-grade = (Math.floor(grade/10)*10);
+// grade = (Math.floor(grade/10)*10);
 
 
-switch(grade) {
-	case 100:
-	case 90:
+// switch(grade) {
+// 	case 100:
+// 	case 90:
+// 	console.log("A")
+// 	break;
+// 	case 80:
+// 	console.log("B")
+// 	break;
+// 	case 70:
+// 	console.log("C")
+// 	break;
+// 	case 60:
+// 	console.log("D")
+// 	break;
+// 	default:
+// 	console.log("F")
+
+// }
+
+switch(true) {
+	case grade >=90:
 	console.log("A")
 	break;
-	case 80:
+	case grade >=80:
 	console.log("B")
 	break;
-	case 70:
+	case grade >=70:
 	console.log("C")
 	break;
-	case 60:
+	case grade >=60:
 	console.log("D")
 	break;
 	default:
 	console.log("F")
-
 }
